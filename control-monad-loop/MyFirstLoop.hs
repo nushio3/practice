998654e7d2bac094cfabc51a998d442a717f0411
Takayuki Muranushi <muranushi@gmail.com>
2012-07-10 22:52:48 +0900

    updated documents

--- control-monad-loop/MyFirstLoop.hs
+++ control-monad-loop/MyFirstLoop.hs
@@ -3,6 +3,7 @@
 module MyFirstLoop where
 import Control.Monad
 import Control.Monad.IO.Class
+import Control.Monad.Trans.Class
 import Control.Monad.Trans.Loop
 
 -- import Control.Monad.Trans.Class    (MonadTrans(lift))
@@ -40,8 +41,8 @@ main1 = do
     liftIO $ putStr $ show i
     when (i >= 9) exit
 
--- | This is main.
--- let us see how it works.
+-- | Let us write some double-loop and
+-- how we can exit from it.
 --
 -- >>> main
 -- (0,0)(0,1)(0,2)(0,3)(0,4)(0,5)(1,0)(1,1)(1,2)
