56f538c114fc73530524e50b50b1ec1090bab7fc
Takayuki Muranushi <muranushi@gmail.com>
2012-07-10 18:01:17 +0900

    the program is now a separate module.

--- control-monad-loop/MyFirstLoop.hs
+++ control-monad-loop/MyFirstLoop.hs
@@ -1,29 +1,31 @@
-> {-# OPTIONS -Wall #-}
-> module Main where
-> import Control.Monad
-> import Control.Monad.IO.Class
-> import Control.Monad.Trans.Loop
-
-Hello, this is a program written in lhs mode.
-First, let's do some include
-and then declare some list.
+{-# OPTIONS -Wall #-}
+module MyFirstLoop where
+import Control.Monad
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Loop
 
 
-> -- | This list contains all integers
-> -- starting from zero. lets check for it.
-> -- 
-> -- >>> print $ take 5 $ xs
-> -- [0,1,2,3,4]
-> --
-> xs :: [Int]
-> xs = [0..]
+-- *introduction 
+{- $
+ Hello, this is a program written to study the 
+ <http://hackage.haskell.org/package/control-monad-loop> package.
+-}
+
+-- | This list contains all integers
+-- starting from zero. lets check for it.
+-- 
+-- >>> print $ take 5 $ xs
+-- [0,1,2,3,4]
+--
+xs :: [Int]
+xs = [0..]
 
 
-> -- | This is main
-> main :: IO ()
-> main = do
->   foreach xs $ \i-> do
->     liftIO $ putStr $ show i
->     when (i >= 9) exit
+-- | This is main
+main :: IO ()
+main = do
+  foreach xs $ \i-> do
+    liftIO $ putStr $ show i
+    when (i >= 9) exit
 
 
