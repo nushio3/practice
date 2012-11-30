69cf91d9042c9bc2415ec4c554e562ac32bb4bd0
Takayuki Muranushi <muranushi@gmail.com>
2012-11-30 20:08:48 +0900

    korede yokattanda.

--- free-objects/applyf.hs
+++ free-objects/applyf.hs
@@ -4,24 +4,9 @@
 
 import Control.Applicative
 
-class ApplyArg f z r where
-  go :: f z -> r
-
-instance (Applicative f) => ApplyArg f z ((z->a) -> f a) where
-  go zs func = func <$> zs
-
-instance (Applicative f, ApplyArg f z ((z->a) -> f a)) => ApplyArg f z (f y -> (y->z->a) -> f a) where
-  go zs ys func = func <$> ys <*> zs
-
-
-xs, ys :: [Int]
-xs = [1..10]
-ys = [1..10]
+class ApplyArg f func ret where
+  go :: func -> ret
 
 
 
-main = print $ (go xs (show) :: [String])
-
---               f b -> (b -> a) -> f a
---        f c -> f b -> (c -> b -> a) -> f a
--- f d -> f c -> f b -> (d ->c -> b -> a) -> f a
+main = print "he"
--- free-objects/zip-vector.hs
+++ free-objects/zip-vector.hs
@@ -25,22 +25,30 @@ instance (Functor f, Zip f, Functor (W f)) => Applicative (W f) where
   (Array fs) <*> (Scalar x) = Array (fmap ($x) fs)
   (Array fs) <*> (Array xs) = Array (zipWith ($) fs xs)
 
-xs, ys, zs, ans :: W V.Vector Int
-xs = Array $ V.fromList [1,2,3]
-ys = Array $ V.fromList [4,5,6]
-zs = Array $ V.fromList [7,8,9]
+liftZ :: Applicative f => f a -> (a -> b) -> f b
+liftZ a fun = pure fun <*> a
 
-ans = liftA3 go xs ys zs where
-  go x y z = 2 * (negate y - 3 * x) + 8 * z
+liftZ2 :: Applicative f => f a -> f b -> (a -> b -> c) -> f c
+liftZ2 a b fun = fun <$> a <*> b
 
+liftZ3 :: Applicative f => f a -> f b -> f c -> (a -> b -> c -> d) -> f d
+liftZ3 a b c fun = fun <$> a <*> b <*> c
 
 backend :: (f a -> f a) -> W f a -> W f a
 backend func (Array xs) = Array (func xs)
 backend func x          = x
 
+
+xs, ys, zs :: W V.Vector Int
+xs = Array $ V.fromList [1,2,3]
+ys = Array $ V.fromList [4,5,6]
+zs = Array $ V.fromList [7,8,9]
+
+main :: IO ()
 main = do
-  print $ ans
-  print $ liftA2 go (backend V.reverse xs) ys where
-     go x y = x * y
-  print $ liftA2 go (backend V.reverse xs) ys where
-     go x y = x * y
+  print $ liftZ3 xs ys zs $
+    \x y z -> 2 * (negate y - 3 * x) + 8 * z
+  print $ liftZ2 (backend V.reverse xs) ys $
+    \x y -> x * y
+  print $ liftZ2 (backend V.reverse xs) ys $
+    \x y -> x + y
