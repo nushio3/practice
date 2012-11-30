67463b7259c5908b41a7581554e145676bbd1334
Takayuki Muranushi <muranushi@gmail.com>
2012-12-01 01:14:57 +0900

    gogogo

---
+++ free-objects/printf5.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverlappingInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE MultiParamTypeClasses#-}
+{-# LANGUAGE UndecidableInstances #-}
+
+-- import qualified Data.Array.Repa as R
+-- import qualified Data.Array.Repa.Eval as R
+
+run :: PType a t => a -> t
+run a0 = spr a0
+
+class PType a t where
+  spr :: a -> t
+
+instance PType a ((a->b)->b) where
+   spr x f = f x
+
+-- instance (Show a) => PType a String where
+--   spr = show
+
+
+instance (PType (a,b) r) => PType a (b->r) where
+  spr a0 = (\b0 -> spr (a0,b0))
+
+
+main = do
+  putStrLn $ run 'c' (3::Int) go
+  putStrLn $ run (5::Double) 'c' (3::Int) goer
+    where
+      go :: (Char, Int) -> String
+      go xs = show xs
+
+      goer :: ((Double, Char),Int) -> String
+      goer xs = show xs
