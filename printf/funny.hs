e74d1723a0fec213d03a007e76a01a70ea285e69
Takayuki Muranushi <muranushi@gmail.com>
2012-12-02 01:10:26 +0900

    gunu

--- printf/funny.hs
+++ printf/funny.hs
@@ -1,17 +1,24 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
 
 import GHC.TypeLits
 
-data Printf (x :: Symbol) = Fmt
+data Printf (x :: [Symbol]) = Fmt
 
-class Printer a b where
+class Printer a b  where
   printf :: Printf a -> b
 
-instance Printer "toge" String where
-  printf _ = "echs found!"
+instance Printer '[] String where
+  printf  _ = ""
+
+instance forall xs. (Printer xs String) => (Printer ("%f" ': xs) (Double->String)) where
+  printf _ x = show x ++ printf (Fmt :: Printf xs)
+
 
 main = do
-  print "hi"     
+  putStrLn $ printf (Fmt :: Printf '["%f"]) (3)
