fe2a79620c6fa22f3146e372c1db963c9961ba8a
Takayuki Muranushi <muranushi@gmail.com>
2012-12-01 19:11:38 +0900

    annotate with outputs.

--- free-objects/zipf-05.hs
+++ free-objects/zipf-05.hs
@@ -82,10 +82,27 @@ longflipZipWithN = spr Nil
 
 main = do
   let args = insert vi1 $ insert vc1 $ insert vd1 Nil
+
   print $ args
+{- 
+fromList [1.1,1.4,1.9] :| (fromList "abc" :| (fromList [100,101,102] :| Nil))
+-}
+
   print $ (reduceFinal vf1 args)
+{-
+fromList ["1.1 a 100","1.4,b,101","1.9-c-102"]
+-}
+
   print $ (longflipZipWithN vd1 vc1 vi1 f_dci_s :: V.Vector String)
+{-
+fromList ["--- K 1.1 I a T 100 A ---","--- K 1.4 I b T 101 A ---","--- K 1.9 I c T 102 A ---"]
+-}
+
   print $ (longflipZipWithN vi1 vd1 f_id_d      :: V.Vector Double)
+{-
+fromList [13780.612339822364,5.740260524389864e14,2.7093636335568943e28]
+-}
+
   putStrLn $ unlines $ V.toList $ 
     longflipZipWithN vd1 vc1 vi1 $ 
     \ d0 c0 i0 ->
@@ -98,3 +115,9 @@ main = do
           s0 = sum [d0,c1,i1] 
           p0 = product [d0,c1,i1]
       in msg
+
+{-
+given 1.1 a 100. Their encode 1.1 33.0 100.0. Sum 134.1. Product 3630.0000000000005.
+given 1.4 b 101. Their encode 1.4 34.0 101.0. Sum 136.4. Product 4807.599999999999.
+given 1.9 c 102. Their encode 1.9 35.0 102.0. Sum 138.9. Product 6783.0.
+-}
