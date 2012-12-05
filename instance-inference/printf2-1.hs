a65c2e8c7683ffca99e65cbadb5ae22bd0ced96f
Takayuki Muranushi <muranushi@gmail.com>
2012-12-05 16:33:13 +0900

    error message added.

--- instance-inference/printf2-1.hs
+++ instance-inference/printf2-1.hs
@@ -17,3 +17,23 @@ main :: IO ()
 main = do
   printf 0.1
   printf "U"
+
+{----
+$ runhaskell printf2-1.hs
+
+printf2-1.hs:18:3:
+    No instance for (PType (IO a0)) arising from a use of `printf'
+    The type variable `a0' is ambiguous
+    Possible fix: add a type signature that fixes these type variable(s)
+    Note: there is a potential instance available:
+      instance PType (IO ()) -- Defined at printf2-1.hs:10:10
+    Possible fix: add an instance declaration for (PType (IO a0))
+    In a stmt of a 'do' block: printf 0.1
+    In the expression:
+      do { printf 0.1;
+           printf "U" }
+    In an equation for `main':
+        main
+          = do { printf 0.1;
+                 printf "U" }
+-----}
