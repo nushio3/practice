bdebfdcf9ebd380b671d29f80ddc769874faa14a
Takayuki Muranushi <muranushi@gmail.com>
2012-12-06 17:19:49 +0900

    smv working on 7.6.1

---
+++ show/storable-mutable-vector.hs
@@ -0,0 +1,28 @@
+
+{-# LANGUAGE ExistentialQuantification, ScopedTypeVariables #-}
+
+import Control.Monad (liftM2)
+import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector.Unboxed.Mutable as MV
+import Data.Vector.Storable.Mutable
+import GHC.Prim (RealWorld)
+import Control.Monad.Primitive
+import Control.Monad
+import qualified Data.Vector as VEC
+import qualified Data.Vector.Generic.Mutable as GM
+import Data.Int
+import Data.Typeable.Internal
+import Data.Primitive
+
+
+data Row m s = Row (MV.MVector (PrimState m) s)
+
+instance  forall m s. (Storable s, MV.Unbox s, Prim s, PrimMonad m) => Show (Row m s) where
+    show (Row row) = do
+      let xx :: m s
+          xx = MV.read row 0
+      "Done"
+
+main :: IO ()
+main = do
+    print "Done"
