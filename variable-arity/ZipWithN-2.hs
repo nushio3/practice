240c6c465fa9c21d874e0861274697322ff13b3a
Takayuki Muranushi <muranushi@gmail.com>
2012-12-08 23:24:33 +0900

    variable arity zip.

--- variable-arity/ZipWithN-2.hs
+++ variable-arity/ZipWithN-2.hs
@@ -2,6 +2,8 @@
            , FunctionalDependencies
            , FlexibleContexts
            , FlexibleInstances
+           , OverlappingInstances
+           , TypeFamilies
            , UndecidableInstances
            #-}
 
@@ -32,23 +34,26 @@
 ----------------------------------------------------------------
 module Data.List.ZipWithN (ZipWithN(), zipWithN) where
 
+import qualified Data.Key as K
+
 -- TODO: add the extra hackery that lets us get rid of the fundeps,
 -- if we can figure out how the heck it works.
--- 
+--
 -- | This class provides the necessary polymorphism. It is only
 -- exported for the sake of giving type signatures.
 --
 -- Because we can't do functor composition without a lot of noise
 -- from newtype wrappers, we use @gr@ and @kr@ to precompose the
 -- direct/list functor with the reader functor and the return type.
-class ZipWithN a gr kr | kr -> gr a where
-    _zipWithN :: [a -> gr] -> [a] -> kr
+class K.Zip f => ZipWithN f a gr kr | kr -> a gr where
+    _zipWithN :: f (a -> gr) -> f a -> kr
 
-instance ZipWithN a b [b] where
-    _zipWithN = zipWith ($)
+instance (K.Zip f, f b ~ fb) => ZipWithN f a b fb where
+    _zipWithN = K.zipWith ($)
 
-instance ZipWithN b gr kr => ZipWithN a (b -> gr) ([b] -> kr) where
-    _zipWithN = (_zipWithN .) . zipWith ($)
+instance (K.Zip f, ZipWithN f b gr kr) =>
+         ZipWithN f a (b -> gr) (f b -> kr) where
+    _zipWithN = (_zipWithN .) . K.zipWith ($)
 
 
 -- | Polyadic version of 'map'/'zipWith'. The given type signature
@@ -60,22 +65,17 @@ instance ZipWithN b gr kr => ZipWithN a (b -> gr) ([b] -> kr) where
 -- Note that the @a@ type variables are meta and so are independent
 -- from one another, despite being correlated in N across all
 -- repetitions.
-zipWithN :: (ZipWithN a gr kr) => (a -> gr) -> [a] -> kr
-zipWithN = _zipWithN . repeat
+--
+-- >>> let f i c d = c : show (i::Int) ++ " " ++ show (d::Double)
+-- >>> zipWithN f [1 :: Int ..] "hoge" [1 :: Double ..] :: [String]
+-- ["h1 1.0","o2 2.0","g3 3.0","e4 4.0"]
+--
+-- >>> zipWithN (*) [1..] [1..10] :: [Int]
+-- [1,4,9,16,25,36,49,64,81,100]
 
-zipWithN1 :: (ZipWithN b gr kr) => (a -> b -> gr) -> [a] -> [b] -> kr
-zipWithN1 func xs = _zipWithN (fmap func xs)
-
+zipWithN :: (ZipWithN f b gr kr)
+            => (a -> b -> gr) -> f a -> f b -> kr
+zipWithN func xs = _zipWithN (fmap func xs)
 
 ----------------------------------------------------------------
 ----------------------------------------------------------- fin.
-
-xs :: [Int]
-xs =  take 10 $ zipWithN (*) [1..] [1..]
-
-
-ys :: [String]
-ys =  take 10 $ zipWithN (\i j -> show i ++ show j) [1..] [1..]
-
-f :: Int ->  Char ->  Double ->  String
-f i c d = (c : show i) ++ show d
