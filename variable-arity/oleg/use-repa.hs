54c71fdea0078aea4a6d70de1c0095ba06ac3c5e
Takayuki Muranushi <muranushi@gmail.com>
2012-12-12 20:53:59 +0900

    oleg's sample for repa.

--- variable-arity/oleg/use-repa.hs
+++ variable-arity/oleg/use-repa.hs
@@ -1,19 +1,28 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeFamilies #-}
+
+{-# OPTIONS -fno-warn-wrong-do-bind #-}
 
 import ZipN
 
+import           Control.Monad
 import qualified Data.Array.Repa as R
 import qualified Data.Array.Repa.Eval as R
 
+
+instance (R.Shape sh) => Functor (R.Array R.D sh) where
+  fmap = R.map
+
 instance (R.Shape sh) => Zip2 (R.Array R.D sh) where
-    zip2 = R.zip
+  zip2 = R.zipWith (,)
 
 type instance IsFunction (R.Array R.D sh) = HFalse
 
 xs, ys, zs :: R.Array R.D R.DIM1 Int
-xs = R.fromList (R.ix1 3) [1,2,3]
-ys = R.fromList (R.ix1 3) [10,11,12]
-zs = R.fromList (R.ix1 3) [100,110,120]
+xs = R.delay $ R.fromListUnboxed (R.ix1 3) [1,2,3]
+ys = R.delay $ R.fromListUnboxed (R.ix1 3) [10,11,12]
+zs = R.delay $ R.fromListUnboxed (R.ix1 3) [100,110,120]
 
 test1 = zipN xs (\x -> (x::Int) + 1)
 -- [2,3,4]
@@ -25,6 +34,8 @@ test3 = zipN xs ys zs (\x y z -> ((x::Int) + y + z))
 -- [111,123,135]
 
 main = do
-  print test1
-  print test2
-  print test3
+  computeAndPrint test1
+  computeAndPrint test2
+  computeAndPrint test3
+    where
+      computeAndPrint = R.computeUnboxedP >=> print
