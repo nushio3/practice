87b8c8f30c58dbe88f8858603d14baa152cde7d4
Takayuki Muranushi <muranushi@gmail.com>
2012-12-14 14:42:05 +0900

    successful binary search.

--- sbv/opt-bin-3.hs
+++ sbv/opt-bin-3.hs
@@ -1,24 +1,47 @@
 import Control.Monad
 import Data.SBV
 
-main2 = do
-  sol <- minimize (Iterative True) 
-   (\[x,y] -> costFunction x y)
-   2
-   (\[x,y] -> constraintFunction x y)
-  print sol
--- seemingly finds x==199, y==1, mincost=3981
-
 main = do
-  (print =<<) $ sat $ forSome ["x","y"] $
-    \x y -> do
-      constrain $ constraintFunction x y
-      return $ costFunction x y .<= 3899
+  let pred thre = forSome ["x","y"] $
+        \x y -> do
+           constrain $ constraintFunction x y 
+           return $ costFunction x y .<= thre
+  tUpper <- unarySearch pred True 1
+  tLower <- unarySearch pred False (-1)
+  binarySearch pred tLower tUpper
 
 
-unarySearch :: (SReal -> Predicate) -> Rational -> IO ()
-unarySearch pred x = do
-  (print =<<) $ sat $ pred x
+unarySearch :: (SReal -> Predicate) -> Bool -> Rational -> IO Rational
+unarySearch pred mode x = do
+  res@(SatResult ans) <- sat $ pred (fromRational x)
+  putStrLn $  "threashold = " 
+           ++ show (fromRational x::Double)
+           ++ " : " ++ show res
+  case ans of
+    Satisfiable _ _ | mode == True  -> return x
+    Unsatisfiable _ | mode == False -> return x
+    _               -> unarySearch pred mode (2*x)
+
+
+binarySearch :: (SReal -> Predicate) -> Rational -> Rational -> IO Rational
+binarySearch pred tLower tUpper = do
+  res@(SatResult ans) <- sat $ pred (fromRational tMid)
+  putStrLn $  "threashold = " 
+           ++ show (fromRational tMid::Double)
+           ++ " : " ++ show res
+  case ans of
+    _ | (tUpper-tLower<1e-20) -> quit
+    Satisfiable _ _ -> binarySearch pred tLower tMid
+    Unsatisfiable _ -> binarySearch pred tMid tUpper
+    _               -> quit
+  where
+    tMid = (tLower + tUpper) / 2
+    quit = do
+      putStrLn "search ends here."
+      return tMid
+      
+  
+
 
 
 costFunction :: SReal ->  SReal -> SReal
