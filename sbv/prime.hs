d90892e234a3237c72c873c50d735b7014623de7
Takayuki Muranushi <muranushi@gmail.com>
2012-12-14 12:50:09 +0900

    sosuu mugennni atta.

--- sbv/prime.hs
+++ sbv/prime.hs
@@ -3,9 +3,17 @@
 import Control.Monad
 import Data.SBV
 
-isPrime :: SInteger -> SBool
-isPrime n = forAll_ $ \k ->
-  (2 .<= k .&& k .< n) ==> n `sMod` k ./= 0
+isPrime :: SInteger -> Predicate
+isPrime n = forAll ["k"] $ \k ->
+  (2 .<= k &&& k .< n) ==> n `sMod` k ./= 0
+
+infinitePrimeTheorem :: Predicate
+infinitePrimeTheorem =
+  forAll ["n"] $ \n ->
+    forSome ["p"] $ \p -> do
+      constrain $ p .>= n
+      isPrime p
+
 
 main = do
   (print =<<) $ prove $ isPrime 2
@@ -13,3 +21,6 @@ main = do
   (print =<<) $ prove $ isPrime 4
   (print =<<) $ prove $ isPrime 5
   (print =<<) $ prove $ isPrime 6
+
+  putStrLn "are there infinite primes?"
+  (print =<<) $ prove $ infinitePrimeTheorem
