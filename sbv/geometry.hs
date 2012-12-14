9e8f55b8b790cd5d8007ae5515805160e3fa6350
Takayuki Muranushi <muranushi@gmail.com>
2012-12-14 16:30:08 +0900

    touch!

---
+++ sbv/geometry.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Data.SBV
+
+main = do
+  (print =<<) $ allSat $ do
+    x <- exists "x"
+    y <- exists "y"
+    constrain $ x^2 + y^2 .== (1::SReal)
+    constrain $ 2*x+3*y   .== 1
+    return (true :: SBool)
+
+  putStrLn ""
+
+  (print =<<) $ allSat $ do
+    x <- exists "x"
+    y <- exists "y"
+    constrain $ x^2 + y^2 .== (1::SReal)
+    constrain $ (x-2)^2 + (y-1)^2 .== 3^2
+    return (true :: SBool)
+
+  putStrLn ""
+
+  (print =<<) $ allSat $ do
+    x <- exists "x"
+    y <- exists "y"
+    constrain $ x^2 + y^2 .== (1::SReal)
+    constrain $ (x-2)^2 + y^2 .== 1
+    return (true :: SBool)
+
+  putStrLn ""
+
+  (print =<<) $ allSat $ do
+    x <- exists "x"
+    y <- exists "y"
+    constrain $ x^2 + y^2 .== (1::SReal)
+    constrain $ (x-3)^2 + y^2 .== 1
+    return (true :: SBool)
