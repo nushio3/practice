2ae087c5ea73435453c1e2370f9cd919d0c9235f
Takayuki Muranushi <muranushi@gmail.com>
2012-12-14 09:56:13 +0900

    output result added.

--- sbv/square-root.hs
+++ sbv/square-root.hs
@@ -14,3 +14,20 @@ main = do
   exercise $ forSome ["x"] $ \(x::SInteger) -> x * x .== 2
   exercise $ forSome ["x"] $ \(x::SReal) -> x * x .== 2
 
+{-
+$ runhaskell square-root.hs
+Q.E.D.
+Satisfiable. Model:
+  x = 2 :: SInteger
+
+Unknown
+Satisfiable. Model:
+  x = 2.0 :: SReal
+
+Falsifiable
+Unsatisfiable
+
+Unknown
+Satisfiable. Model:
+  x = root(1, x^2 = 2) = -1.414213562373095... :: SReal
+-}
