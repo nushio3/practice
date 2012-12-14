d66687bebdac13bad27624aaadc9327c5fc2a19f
Takayuki Muranushi <muranushi@gmail.com>
2012-12-14 15:41:41 +0900

    rects

--- sbv/opt-bin-pi.hs
+++ sbv/opt-bin-pi.hs
@@ -1,59 +1,34 @@
+{-# LANGUAGE RecordWildCards #-}
+
 import Control.Monad
 import Data.SBV
 
+type Figure = SReal -> SReal -> SBool
+
+circle :: Figure
+circle x y = x^2 + y^2 .<= 1
+
+data Rectangle = Rectangle
+  { lef :: Rational, bot :: Rational, rig :: Rational, top :: Rational }
+
+isAllIn, isAllOut :: Rectangle -> Figure -> Predicate
+
+isAllIn Rectangle{..} fig =
+  forAll ["x","y"] $ \x y ->
+    (fromRational lef .<= x &&& x .<= fromRational rig &&&
+     fromRational bot .<= y &&& y .<= fromRational top)
+       ==> fig x y
+
+isAllOut Rectangle{..} fig =
+  forAll ["x","y"] $ \x y ->
+    (fromRational lef .<= x &&& x .<= fromRational rig &&&
+     fromRational bot .<= y &&& y .<= fromRational top)
+       ==> bnot (fig x y)
+
+
+
 main = do
-  let pred thre = forSome ["x","p"] $
-        \x p -> do
-           constrain =<< constraintPred x p
-           return $ costFunction x p .<= thre
-  tUpper <- unarySearch pred True 1
-  tLower <- unarySearch pred False (-1)
-  binarySearch pred tLower tUpper
-
-
-unarySearch :: (SReal -> Predicate) -> Bool -> Rational -> IO Rational
-unarySearch pred mode x = do
-  res@(SatResult ans) <- sat $ pred (fromRational x)
-  putStrLn $  "threashold = "
-           ++ show (fromRational x::Double)
-           ++ " : " ++ show res
-  case ans of
-    Satisfiable _ _ | mode == True  -> return x
-    Unsatisfiable _ | mode == False -> return x
-    _               -> unarySearch pred mode (2*x)
-
-
-binarySearch :: (SReal -> Predicate) -> Rational -> Rational -> IO Rational
-binarySearch pred tLower tUpper = do
-  res@(SatResult ans) <- sat $ pred (fromRational tMid)
-  putStrLn $  "threashold = "
-           ++ show (fromRational tMid::Double)
-           ++ " : " ++ show res
-  case ans of
-    _ | (tUpper-tLower<1e-20) -> quit
-    Satisfiable _ _ -> binarySearch pred tLower tMid
-    Unsatisfiable _ -> binarySearch pred tMid tUpper
-    _               -> quit
-  where
-    tMid = (tLower + tUpper) / 2
-    quit = do
-      putStrLn "search ends here."
-      return tMid
-
-
-
-
-
-costFunction :: SReal -> SReal -> SReal
-costFunction x p = p
-
-constraintPred :: SReal -> SReal -> Predicate
-constraintPred x p = do
-  constrain $ p.>=0
-  constrain $ p^4/90 .>= x
-  forAll ["n"] $ \n -> termSum n .<= x
-    where
-      term :: SInteger -> SReal
-      term n = 1/ (toSReal $ n^4)
-      termSum :: SInteger -> SReal
-      termSum n = ite (n .<= 0) 0 (termSum (n-1) + term n)
+  (print =<<) $ prove $ Rectangle 0 0 0.5 0.5 `isAllIn` circle
+  (print =<<) $ prove $ Rectangle 1 1 2 2 `isAllOut` circle
+  (print =<<) $ prove $ Rectangle 0 0 1 1 `isAllOut` circle
+  (print =<<) $ prove $ Rectangle 0 0 1 1 `isAllIn` circle
