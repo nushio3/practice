d2771bc311618484ae1c41896e2a5397d370e3a2
Takayuki Muranushi <muranushi@gmail.com>
2012-12-25 23:14:20 +0900

    gogo nagoya

--- duck/Duck.hs
+++
@@ -1,6 +0,0 @@
-import qualified Data.Map as Map
-import           Data.Dynamic
-
-module Duck where
-
-newtype Object = Object{Map.Map TypeRep Dynamic}
---
+++ duck/examples/test-01.hs
@@ -0,0 +1,6 @@
+
+
+import Duck
+
+main = do
+  print "hi"
