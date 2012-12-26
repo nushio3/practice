e1a6c5d4298e66a8dbc92f75f6bf36f1cf5d8f08
Takayuki Muranushi <muranushi@gmail.com>
2012-12-26 23:06:34 +0900

    single duck found.

--- duck/examples/test-01.hs
+++ duck/examples/test-01.hs
@@ -1,12 +1,13 @@
 import           Control.Lens
 import           Control.Monad
+import           Data.List (isInfixOf)
 import           Data.Object
 import           Data.Object.Flying
 import qualified Data.Object.Wav as Wav
 import           Data.Maybe
 import qualified Data.Vector as V
 
-x1,x2,x3,x4,x5 :: Object
+x1,x2,x3,x4,x5, santa :: Object
 x1 = empty
 
 x2 = x1 & speed .~ Just 120
@@ -17,6 +18,10 @@ x4 = x3 & over speed (fmap (*2)) -- accelerate duck!
 
 x5 = x3 & Wav.sound .~ Just (V.generate 44100 (\i -> floor (sin(2*pi*800 * fromIntegral i/44100))))
 
+santa = empty
+ & speed .~ Just 64000
+ & sound .~ Just "Merry Xmas! Hohohoho!"
+
 main = do
   print x1
   print $ x1 ^. speed
@@ -25,8 +30,10 @@ main = do
   print $ x3 ^. sound
   let speeders :: [Object]
       speeders = do
-        x <- [x1, x2, x3, x4, x5]
-        s <- maybeToList $ x ^. speed
-        guard $ s > 200
+        x <- [x1, x2, x3, x4, x5, santa]
+        sp <- maybeToList $ x ^. speed
+        snd <- maybeToList $ x ^. sound
+        guard $ sp > 200
+        guard $ "quack" `isInfixOf` snd
         return x
   print speeders
