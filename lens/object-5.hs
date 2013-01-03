48646471b8325e79d0af2b2f75b0bb27a40479ec
Takayuki Muranushi <muranushi@gmail.com>
2013-01-03 19:28:19 +0900

    drilling

--- lens/object-5.hs
+++ lens/object-5.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE RankNTypes #-}
 import           Control.Applicative hiding (empty)
 import           Control.Lens
+import           Data.Maybe
 import qualified Data.Map as Map
 import           Text.Printf
 
@@ -25,7 +26,7 @@ memberWithDefault tag defFunc d2ad obj@(Object kvp) =
        go d = (\d' -> Object $ Map.insert tag d' kvp) <$> (d2ad d)
 
 
-mass, velocity, momentum ::  Simple Traversal Object Double
+mass, velocity, momentum, energy ::  Simple Traversal Object Double
 mass = member "mass"
 velocity = memberWithDefault "velocity" $ \this -> do
   m <- this ^? mass
@@ -37,23 +38,39 @@ momentum = memberWithDefault "momentum" $ \this -> do
   v <- this ^? velocity
   return (m*v)
 
+energy = memberWithDefault "energy" $ \this -> do
+  m <- this ^? mass
+  v <- this ^? velocity
+  return $ 0.5 * m * v * v
+
 
 insert :: String -> Double -> Object -> Object
 insert tag d (Object kvp) = Object $Map.insert tag d kvp
 
-empty, x, y :: Object
+empty, hammer1, hammer2, laser :: Object
 empty = Object Map.empty
-x = empty & insert "mass" 30
-          & insert "velocity" 40
 
-y = empty & insert "mass" 50
-          & insert "momentum" 1500
+-- mass and impact velocity is known for this hammer
+hammer1 = empty & insert "mass" 30
+                & insert "velocity" 40
+
+-- mass and momentum is known for this hammer
+hammer2 = empty & insert "mass" 50
+                & insert "momentum" 1500
+
+-- mass and velocity is not defined for laser
+laser = empty & insert "energy" 26500
+
 
 main = do
-  print empty               -- Object {unObject = fromList []}
-  print x                   -- Object {unObject = fromList [("mass",30.0),("velocity",40.0)]}
-  print $ x ^? velocity     -- Just 40.0
-  print $ x ^? momentum     -- Just 1200.0
-  print $ y ^? velocity     -- Just 30.0
-  print $ y ^? momentum     -- Just 1500.0
-  print $ empty ^? momentum -- Nothing
+  print empty                  -- Object {unObject = fromList []}
+  print hammer1                -- Object {unObject = fromList [("mass",30.0),("velocity",40.0)]}
+  print $ hammer1 ^? velocity  -- Just 40.0
+  print $ hammer1 ^? momentum  -- Just 1200.0
+  print $ hammer2 ^? velocity  -- Just 30.0
+  print $ hammer2 ^? momentum  -- Just 1500.0
+  print $ empty ^? momentum    -- Nothing
+
+  -- what is the most powerful tool for drilling?
+  let energies = mapMaybe (^? energy) [empty,hammer1,hammer2,laser]
+  print $ energies
