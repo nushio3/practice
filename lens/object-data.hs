24713cdb8dbee655548edcc51def7822b36f9fbe
Takayuki Muranushi <muranushi@gmail.com>
2013-01-03 23:54:20 +0900

    polymorphic object using usual data-type.

--- lens/object-data.hs
+++ lens/object-data.hs
@@ -1,5 +1,15 @@
 
 data Object a = Object
   { mass :: a
-  , 
+  , velocity :: a 
   }
+
+momentum :: Num a => Object a -> a
+momentum obj = mass obj * velocity obj
+
+x :: Num a => Object a
+x = Object 6 7
+
+main = do
+  print $ 1000 * momentum x
+  print $ 1e41 * momentum x
