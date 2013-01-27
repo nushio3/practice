c035f1d697153ef7297c804286adf34221d8b6f2
Takayuki Muranushi <muranushi@gmail.com>
2013-01-28 00:27:45 +0900

    proven the first two instructions.

--- sbv/satic/AsmSpec.hs
+++ sbv/satic/AsmSpec.hs
@@ -1,5 +1,7 @@
+import           Data.Bits
 import qualified Data.SBV as SBV
-import Test.Hspec
+import           Data.SBV ((.==), (.<), (.<=), (&&&))
+import           Test.Hspec
 
 import Asm
 
@@ -10,7 +12,7 @@ shouldProve theorem = do
   res <- SBV.prove theorem            
   show res `shouldBe` "Q.E.D."
 
-allEqual xs ys = SBV.bAnd $ zipWith (SBV..==) xs ys
+allEqual xs ys = SBV.bAnd $ zipWith (.==) xs ys
 
 spec :: Spec
 spec = do
@@ -23,9 +25,13 @@ spec = do
   describe "Ial" $ do
     it "sets the last 4 bits of eax." $ do
       shouldProve $ do
-        rs0 <- symbolicRegs          
-        rs1 <- exec (toHex $ Ial 0) rs0
-        return $ allEqual rs0 rs1
+        rs0 <- symbolicRegs       
+        n <- SBV.sWord8 "n"   
+        SBV.constrain $ 0 .<= n &&& n .< 16
+        rs1 <- exec (toHex $ Ial n) rs0
+        let (rs0h: rs0t) = rs0
+            rs0' = ((rs0h .&. 0xf0) .|. n) : rs0t
+        return $ allEqual rs0' rs1
     
        
        
