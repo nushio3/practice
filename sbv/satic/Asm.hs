aae23454f8f8a28769e5800d88510ef076a9609e
Takayuki Muranushi <muranushi@gmail.com>
2013-01-27 21:03:46 +0900

    so far so good. now lets use select extensively.

--- sbv/satic/Asm.hs
+++ sbv/satic/Asm.hs
@@ -1,3 +1,37 @@
+import Data.SBV
+
+type SReg = SWord8
+type SRegArray = SWord64
+type SInst = SWord8
+
+nReg :: Num a => a
+nReg = 8
+
+bitPerReg :: Num a => a
+bitPerReg = 8
+
+
+exec :: SInst -> SRegArray -> Symbolic SRegArray
+exec inst rs = do
+  let opCode = inst `shiftR` 4
+      arg = inst .&. 0xf
+      lhs = (rs `sbvShiftRight` (bitPerReg * arg)) .&. 0xff
+      rsa0 = (rs .&. (-1-0xff)) -- rs with x[0] cleared
+      cands =
+        [ -- Nop
+          rs
+          -- Ial
+        , (rs .&. (-1-0xf))  .|. (extend $ extend $ extend $ arg)
+          -- Iah
+        , (rs .&. (-1-0xf0)) .|. (extend $ extend $ extend $ arg `shiftL` 4)
+          -- Cpy
+        , rsa0 .|. lhs
+          -- Xch
+        , rsa0 .&. (-1 - (0xff `sbvShiftLeft` (bitPerReg * arg)))
+        ]
+
+  return $ select cands rs opCode
+
 data Asm a
   = Nop   -- 00   do nothing
   | Ial a -- 1r   set lower 4 bit of x[0]
@@ -16,6 +50,8 @@ data Asm a
   | Mul a -- Er   x[0] *= x[r]
   | Fma a -- Fr   x[0] += x[r] * x[r+1]
     deriving (Eq, Show)
+
+
 
 main = do
   print "hi"
