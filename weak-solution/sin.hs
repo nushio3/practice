9ca5960e06f40c5e461c8668455c850810c8626c
Takayuki Muranushi <muranushi@gmail.com>
2013-03-01 19:26:35 +0900

    interesting sinudal optimization.

---
+++ weak-solution/sin.hs
@@ -0,0 +1,40 @@
+import qualified Numeric.Optimization.Algorithms.CMAES as Opt
+
+main = do
+  let conf = Opt.minimize noise $ replicate 8 0
+  xs <- Opt.run $ conf -- {Opt.verbose = True}
+  print xs
+
+noise :: [Double] -> Double
+noise args = sum $ map r2n ranges
+  where
+    r2n (a, b) =
+        (f b - f a - integ b a g)^2 +
+        (g b - g a + integ b a f)^2
+      where f = poly fargs
+            g = poly gargs
+
+    ranges = [let x = fromIntegral i / fromIntegral nDiv
+              in (x*2*pi, (x+1)*2*pi)
+             | i<-[0..nDiv]]
+
+    poly :: [Double] -> Double -> Double
+    poly as x = sum $ zipWith (\a p -> a * x^p) as [0..]
+
+    fargs = (1:) $ map snd $ filter (even . fst) $ zip [0..] args
+    gargs = (0:) $ map snd $ filter (odd  . fst) $ zip [0..] args
+
+    nDiv :: Int
+    nDiv = 100
+
+    nIntegral :: Int
+    nIntegral = 100
+
+    integ :: Double ->  Double -> ( Double -> Double ) -> Double
+    integ bot top f =
+      let dx = (top-bot) / fromIntegral nIntegral in
+        sum $
+        map (\(w,x) -> w * dx * f x) $
+        map (\i -> (if i==0 || i==nIntegral then 0.5 else 1,
+                    dx * fromIntegral i)) $
+        [0..nIntegral]
