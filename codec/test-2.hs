c2bf52e3141b4bd3214e1adae81f39e806ef22a6
Takayuki Muranushi <muranushi@gmail.com>
2013-03-16 23:06:08 +0900

    codec complete

--- codec/test-2.hs
+++ codec/test-2.hs
@@ -1,36 +1,54 @@
 {-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 import GHC.Exts
 
 import qualified Data.Binary as Bin
--- import qualified Data.Aeson as Aeson
+import qualified Data.Aeson as Aeson
 import qualified Data.ByteString.Lazy.Char8 as BS
 
 
 data CodecProxy (x :: * -> Constraint) = CodecProxy
 
-type family GetCodec p :: ( * -> Constraint)
-type instance GetCodec (CodecProxy x) = x
+type family GetConstr p :: ( * -> Constraint)
+type instance GetConstr (CodecProxy x) = x
 
-class Codec (x :: * -> Constraint) where
-  type Code x :: *
-  encode :: (x a) => a -> Code x
-  decode :: (x a) => Code x -> Maybe a
+class Codec p where
+  type Code p :: *
+  encode :: ((GetConstr p) a) => p -> a -> Code p
+  decode :: ((GetConstr p) a) => p -> Code p -> Maybe a
 
-instance Codec (Bin.Binary) where
-  type Code Bin.Binary = BS.ByteString
-  encode = Bin.encode
-  decode = Just . Bin.decode
-
-
-suepyon :: (Codec x) => x a => a -> Code x
-suepyon = undefined
+instance Codec (CodecProxy Bin.Binary) where
+  type Code (CodecProxy Bin.Binary) = BS.ByteString
+  encode _ = Bin.encode
+  decode _ = Just . Bin.decode
 
 
+class (Read a, Show a) => ReadShow a where
+
+instance (Read a, Show a) => ReadShow a where
+
+
+
+instance Codec (CodecProxy ReadShow) where
+  type Code (CodecProxy ReadShow) = String
+  encode _ = show
+  decode _ = Just . read
+
+
+suepyon :: (Codec p, (GetConstr p) a, Show a, Show (Code p))
+           => p -> a -> IO ()
+suepyon px a0 = do
+  print a0
+  print $ encode px a0
+  print $ (decode px $ encode px a0) `asTypeOf` (Just a0)
 
 main :: IO ()
-main = print 1 -- $ (encode) (1::Int)
+main = do
+  suepyon (CodecProxy :: CodecProxy Bin.Binary) (4.2 :: Double)
+  suepyon (CodecProxy :: CodecProxy ReadShow) (4.2 :: Double)
