d30ab6ebbc58b2cdb59215630bf815ae950a0aed
Takayuki Muranushi <muranushi@gmail.com>
2013-06-14 23:22:42 +0900

    practice extrasolar planet detection.

--- exoplanet.eu/convert.hs
+++ exoplanet.eu/convert.hs
@@ -1,6 +1,34 @@
 #!/usr/bin/env runhaskell
 
+import Control.Monad
+import Data.List (isPrefixOf)
+import Data.Maybe (catMaybes)
+import Data.String.Utils (split)
+import Safe (readMay, atMay)
+import Text.Printf
+
+readMR :: String -> Maybe (Double, Double)
+readMR str = do
+  m <- atMay ws 1 >>= readMay
+  r <- atMay ws 2 >>= readMay
+  return (m / mJPerME, r / rJPerRE)
+  where        
+    ws = split "," str
+    mJPerME = 1 / 317.828133
+    rJPerRE = 6371 / 69911 -- mean radii, (2a+b)/3
+
 
 main :: IO ()
 main = do
   str <- readFile "exoplanet.csv"
+  let pcat :: [String]
+      pcat = 
+        filter (not . isPrefixOf "#") $
+        lines str
+      mrs = catMaybes $ map readMR pcat
+  forM_ mrs $ \(m,r) ->
+    printf "%f %f\n" m r
+    
+  
+  
+  
---
+++ exoplanet.eu/plot.gnu
@@ -0,0 +1,5 @@
+set term postscript enhanced color solid
+set grid
+set out "mr.eps"
+set log x
+plot "mr.txt"
