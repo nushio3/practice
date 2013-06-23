d5263bab124199e9e3a8348074a3e52bcdbbd965
Takayuki Muranushi <muranushi@gmail.com>
2013-06-23 13:09:25 +0900

    recursion example using fix-point operator.

--- recursion-schemes/FibTest.hs
+++ recursion-schemes/FibTest.hs
@@ -4,14 +4,26 @@ import Test.QuickCheck.Arbitrary (Arbitrary(..))
 import Test.Hspec
 import Test.Hspec.QuickCheck (prop)
 
+-- Straightfoward definition of fibonacci numbers
+
 fib :: Integer -> Integer
 fib n
-  | n <= 2    = 1
+  | n <= 1    = 1
   | otherwise = fib (n-1) + fib (n-2)
+
+-- Memoized fib using list
+
+fibL :: [Integer]
+fibL = 1:1: zipWith (+) fibL (tail fibL)
+
+fibWithL :: Integer -> Integer
+fibWithL n = fibL !! fromIntegral n
+
+-- fib in continuation passing style.
 
 fibCPS :: Integer -> (Integer -> a) -> a
 fibCPS n k
-  | n <= 2    = k 1
+  | n <= 1    = k 1
   | otherwise = fibCPS (n-1) $
                 \val1 -> fibCPS (n-2) $
                          \val2 -> k (val1 + val2)
@@ -19,18 +31,35 @@ fibCPS n k
 fibWithCPS :: Integer -> Integer
 fibWithCPS n = fibCPS n id
 
+-- fib defined using the recursion primitives.
 
-data FibStream a = Done a
-                 | StillWorking Integer (Integer -> a)
+fibAbs :: (Integer -> Integer) -> Integer -> Integer
+fibAbs fib n
+  | n <= 1 = 1
+  | otherwise = fib (n-1) + fib (n-2)
 
+fix :: (a->a)->a
+fix f = f (fix f)
+
+fibWithFix :: Integer -> Integer
+fibWithFix = fix fibAbs
+
+
+-- utilitiy for testing
 newtype Small = Small Integer deriving (Show)
 instance Arbitrary Small where
-  arbitrary = fmap (Small . (`mod` 32)) arbitrary
+  arbitrary = fmap (Small . (`mod` 30)) arbitrary
   shrink = const []
 
 
+-- main test routine
+
 main :: IO ()
 main = hspec $ do
-  describe "fibonacci in cps" $ do
-    prop "matches the original fib" $ \(Small n) ->
+  describe "fibonacci" $ do
+    prop "cps style matches the original fib" $ \(Small n) ->
       fib n == fibWithCPS n
+    prop "list style matches the original fib" $ \(Small n) ->
+      fib n == fibWithL n
+    prop "fix style matches the original fib" $ \(Small n) ->
+      fib n == fibWithFix n
