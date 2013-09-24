0d77f43e8a8775c13f4a3088329cff68bf591271
Takayuki Muranushi <muranushi@gmail.com>
2013-09-25 01:09:30 +0900

    we can now do this at typelevel! yey!

--- type-nats/nat-fib.hs
+++ type-nats/nat-fib.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 import GHC.TypeLits
 
@@ -8,10 +11,16 @@ type family Fib (n :: Nat) :: Nat where
   Fib 1 = 1
   Fib n = Fib (n-1) + Fib (n-2)
   
+type family Fac (n :: Nat) :: Nat where
+  Fac 0 = 1
+  Fac n = n * Fac (n-1)
+
+
 main = do
   print (sing :: Sing 3)
   print (sing :: Sing (8*9))  
   print (sing :: Sing (Fib 10))    
---  print (sing :: Sing (2^100))  
+  print (sing :: Sing (Fac 10))    
+  print (sing :: Sing (2^100))  
   
   
