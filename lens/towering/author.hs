7c24ae4c3f084f511e921f3fd4ca1b593e72f9be
Takayuki Muranushi <muranushi@gmail.com>
2013-09-27 19:51:02 +0900

    successful partial write to composite monoid.

--- lens/towering/author.hs
+++ lens/towering/author.hs
@@ -15,6 +15,20 @@ deriving instance Monoid LaTeX
 
 makeClassy ''LaTeX
 
+data Document = Document { _docLaTeX :: LaTeX, _docSize :: Int}
+  deriving (Eq, Show)                
+                
+makeClassy ''Document
+instance HasLaTeX Document where laTeX = docLaTeX
+
+instance Monoid Document where
+  mempty = Document (LaTeX "") 0
+  mappend (Document s0 n0) (Document s1 n1) 
+    = Document (mappend s0 s1) (n0 + n1)
+  
+
+
+
 prog :: StateT LaTeX IO ()
 prog = do
   laText .= "Hi"
@@ -25,11 +39,19 @@ prog2 = do
   scribe laTeX $ LaTeX "From lens with LaTeX."  
   scribe laText "From lens with String."
 
+prog3 :: (HasLaTeX s, MonadWriter s m) => m ()
+prog3 = do
+  scribe laText "To MonadWriter from lens with String."  
+
 main :: IO ()
 main = do
   ((),ret) <- runStateT prog (LaTeX "")
   print ret
   ((),ret2) <- runWriterT prog2
   print ret2
+  ((), ret3) <- runWriterT prog3
+  print (ret3 :: LaTeX)
+  ((), ret4) <- runWriterT prog3
+  print (ret4 :: Document)
   
   
