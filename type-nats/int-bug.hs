ea8a1716b1d6221ce32d77432b6de3f38e653a27
Takayuki Muranushi <muranushi@gmail.com>
2013-10-15 15:03:40 +0900

    add comments

--- type-nats/int-bug.hs
+++ type-nats/int-bug.hs
@@ -14,10 +14,20 @@ type family Diff (x :: Nat) (y :: Nat) :: Nat where
 
 main :: IO ()
 main = do
+  putStrLn "comparisons are working for zeros and non-zero numbers"
+  
   print $ fromSing (sing :: Sing (If (4 <=? 1) True False))
   print $ fromSing (sing :: Sing (If (1 <=? 4) True False))
   print $ fromSing (sing :: Sing (If (4 <=? 0) True False))
   print $ fromSing (sing :: Sing (If (0 <=? 4) True False))
+  
+  putStrLn "subtractions are working for zeros and non-zero numbers"  
+  
+  print $ fromSing (sing :: Sing (4 - 1))  
+  print $ fromSing (sing :: Sing (4 - 0))
+  
+  putStrLn "But if you combine them.."    
+  
   print $ fromSing (sing :: Sing (Diff 4 1))
   print $ fromSing (sing :: Sing (Diff 1 4))
   --print $ fromSing (sing :: Sing (Diff 4 0)) -- this doesn't work
