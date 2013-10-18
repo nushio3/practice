a4a8c353d91ac9faf3e60ca879dd13678fe18dc9
nushio <nushio@nushiohshio>
2013-10-18 01:36:26 +0000

    antiquoted.

--- template-haskell/Aqlang.hs
+++ template-haskell/Aqlang.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+
 module Aqlang where
 
 import Language.Haskell.TH
@@ -7,5 +10,21 @@ import Language.Haskell.TH
 import Language.Haskell.TH.Quote
  
 doc :: QuasiQuoter
-doc = QuasiQuoter { quoteExp = stringE }
+doc = QuasiQuoter { 
+  quoteExp = joinE . map cvtE . parseE ,
+  quotePat = error "doc is defined only for expression context" ,  
+  quoteType = error "doc is defined only for expression context" ,  
+  quoteDec = error "doc is defined only for expression context" 
+  }
 
+parseE :: String -> [String]
+parseE = words
+
+cvtE :: String -> ExpQ
+cvtE ('`':nam) = varE $ mkName nam
+cvtE x         = stringE x
+
+joinE :: [ExpQ] -> ExpQ
+joinE = foldl ap [e| "" |] 
+  where
+    ap a b = appE (appE (varE '(++) ) a ) b
--- template-haskell/AqlangMain.hs
+++ template-haskell/AqlangMain.hs
@@ -1,11 +1,11 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 import Aqlang
 
-xs :: String
-xs = [doc| Hello if case as nyan! |]
 
 main :: IO ()
 main = do
-  putStrLn xs
+  let nyan = "28-28-"
+  putStrLn [doc| Hello if case as `nyan ! |]
   
