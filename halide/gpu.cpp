7535518c69aec453cd76b38979601eddc8615989
nushio <nushio@nuwheezy.paraiso-lang.org>
2014-06-14 19:52:10 +0900

    learn how to generate gpu.

---
+++ halide/gpu.cpp
@@ -0,0 +1,23 @@
+// On linux, you can compile and run it like so:
+// g++ lesson_01*.cpp -g -I ../include -L ../bin -lHalide -o lesson_01
+// export LD_LIBRARY_PATH=./Halide/bin/ HL_DEBUG_CODEGEN=1  HL_JIT_TARGET=ptx
+
+#include <Halide.h>
+
+// We'll also include stdio for printf.
+#include <stdio.h>
+using namespace Halide;
+
+int main(int argc, char **argv) {
+
+  Func gradient("gradient_gpu");
+  Var x("x"), y("y");
+
+  gradient(x, y) = (0.0f + x) * y + 42.195f;
+  gradient.gpu_tile(x, y, 4,4);
+  
+  
+  Image<int> output = gradient.realize(4, 4);
+
+  return 0;
+}
