261fb9abd7079ad37bdbd1a28482aa04773be88e
Your Name <you@example.com>
2014-07-08 17:20:29 +0900

    committa hanashi ya

--- halide/bench-blur.cpp
+++ halide/bench-blur.cpp
@@ -3,13 +3,19 @@
 #include <vector>
 #include <fstream>
 #include <stdlib.h>
+#include <sys/time.h>
 
- 
+namespace{
+  double wtime() {
+    timeval tv;
+    gettimeofday(&tv,NULL);
+    return double(tv.tv_sec) + double(tv.tv_usec)*1e-6;
+  }
+}
+
 void bench(int NX, int NY, int MAX_T) {
 
-
-
-  Halide::Var x,y;
+  Halide::Var x,y, yo, yi;
   Halide::Func initial_condition("initial_condition");
   initial_condition(x, y) = 0.0f;
   initial_condition(NX/3, NY/4) = 1.0f* NX*NY ;
@@ -22,13 +28,18 @@ void bench(int NX, int NY, int MAX_T) {
 
   // precompile the programs
   Halide::Func cell2, cell3; const float a = 0.5f, b = 0.25f;
-  cell2(x,y)= (a * inPar(x,y) + b * inPar((x+1)%NX,y) + b * inPar((x+NX-1)%NX,y)) ;
-  cell3(x,y)= (a * cell2(x,y) + b * cell2(x,(y+1)%NY) + b * cell2(x,(y+NY-1)%NY)) ;
+  cell2(x,y)= (a * inPar(x,y) + b * inPar(clamp(x+1,0,NX-1),y) + b * inPar(clamp(x+2,0,NX-1),y)) ;
+  cell3(x,y)= (a * cell2(x,y) + b * cell2(x,clamp(y+1,0,NY-1)) + b * cell2(x,clamp(y+2,0,NY-1))) ;
+  // cell3.parallel(y);
+
+  cell3.split(y, yo, yi, 16).parallel(yo).parallel(yo).vectorize(x,4);
+  cell2.store_at(cell3,yo).compute_at(cell3,yi).vectorize(x,4);
   
   for (int t=0; t<=MAX_T; ++t) {
     // updating logic
     inPar.set(input);
     output=cell3.realize(NX,NY);
+
 
     // swap the double-buffer
     std::swap(input, output);
@@ -51,5 +62,22 @@ void bench(int NX, int NY, int MAX_T) {
 
 
 int main(int argc, char **argv) {
-  bench(1200,800,400);
+  using std::cout;
+  using std::cerr;
+  // uisng std::endl;
+  
+
+  for (size_t ny = 16; ny < (2<<15); ny*=2) {
+    for (size_t nx = 16; nx < (2<<15); nx*=2) {
+      if (nx*ny >= (2<<28)) continue;
+
+      for (size_t t_max = 1; t_max <= 1000;t_max *= 10) {
+	
+	double t0 = wtime();
+	bench(nx,ny,t_max);
+	double t1 = wtime();
+	cout << nx << " " << ny << " " << t_max << " " << (t1-t0) << std::endl;
+      }
+    }
+  }
 }
