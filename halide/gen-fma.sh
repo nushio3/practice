2272d3280bd1a4c838c4918be5e5913b9bc1dd3b
nushio <nushio@nuwheezy.paraiso-lang.org>
2014-07-08 10:40:24 +0900

    update the slides.

--- halide/gen-fma.sh
+++ halide/gen-fma.sh
@@ -1,5 +1,6 @@
 # for more natural, 4-way fma
-clang -O1 -march=bdver1 -mfma4 -ffp-contract=fast  blur.bc -S
+clang -O1 -march=bdver1 -ffp-contract=fast blur.bc -S
+#clang -O1 -march=bdver1 -mfma4 -ffp-contract=fast  blur.bc -S
 
 # for 3-way fma
 # clang -O3 -march=core-avx2 -mfma -ffp-contract=fast  blur.bc -S
