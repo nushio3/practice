59ae061452b5e4b9195a8f78ea9f5283ee9351a4
Your Name <you@example.com>
2014-07-22 16:00:35 +0900

    unroll factor is important

--- halide/blur-step-fusion.cpp
+++ halide/blur-step-fusion.cpp
@@ -15,7 +15,7 @@ namespace{
 
 const int N_FUSION = 4;
 
-void bench(int NX, int NY, int MAX_T) {
+double bench(int NX, int NY, int MAX_T) {
 
   Halide::Var x("x"),y("y"), yo("yo"), yi("yi"), xo("xo"), xi("xi");
   Halide::Func initial_condition("initial_condition");
@@ -47,16 +47,32 @@ void bench(int NX, int NY, int MAX_T) {
   }
 
 
-  
+  Halide::Var nid("nid");
   for(int i_f = N_FUSION-1; i_f >=0 ; --i_f) {
-    if (i_f==N_FUSION-1)
-      cell3[i_f].split(y, yo, yi, NY/64).parallel(yo).vectorize(x,4);
-    // else
-    //  cell3[i_f].store_at(cell2[i_f+1], yo).compute_at(cell2[i_f+1],yi).vectorize(x,4);
-    //cell2[i_f].store_at(cell3[i_f], yo).compute_at(cell3[i_f],yi).vectorize(x,4);
+    if (i_f==N_FUSION-1){
+      cell3[i_f].tile(x,y, xo,yo, xi, yi, NX/32,NY/32).fuse(xo,yo,nid).parallel(nid).vectorize(xi,8).unroll(xi,4);
+    }
+    else
+      cell3[i_f].store_at(cell3[N_FUSION-1],nid).compute_at(cell3[N_FUSION-1], yi).vectorize(x,8).unroll(x,4);
+    cell2[i_f].store_at(cell3[N_FUSION-1], nid).compute_at(cell3[N_FUSION-1], yi).vectorize(x,8).unroll(x,4);
   }
 
 
+  std::cerr << "all strategy set"<< std::endl;    
+  
+
+  // run the computation once to generate the program
+  inPar.set(input);
+  output=cell3[N_FUSION-1].realize(NX,NY);
+  std::swap(input, output);
+  {
+    std::vector<Halide::Argument> arg_vect;
+    arg_vect.push_back(Halide::Argument("inPar", true, Halide::Int(32)));
+    cell3[N_FUSION-1].compile_to_assembly("blur-fusion-gen.s", arg_vect, "blur");
+  }
+
+
+  double t0 = wtime();
   
   for (int t=0; t<=MAX_T; ++t) {
     // updating logic
@@ -66,6 +82,8 @@ void bench(int NX, int NY, int MAX_T) {
     std::swap(input, output);
   }
 
+  double t1 = wtime();
+  
   // output the final state
   {
     std::ofstream ofs("debug.txt");
@@ -78,6 +96,8 @@ void bench(int NX, int NY, int MAX_T) {
     ofs << ret << std::endl;
     ofs.close();
   } 
+
+  return t1-t0;
 }
 
 
@@ -89,15 +109,13 @@ int main(int argc, char **argv) {
   // uisng std::endl;
 
 
-  for (size_t t_max=1; ; t_max*=2) {
+  for (size_t t_max=256; ; t_max*=2) {
     size_t ny = 2<<10;
     size_t nx = 2<<10;
-    double t0 = wtime();
-    bench(nx,ny,t_max);
-    double t1 = wtime();
-    double num_flop =  double(nx) * double(ny) * double(t_max) * 10 *  N_FUSION;
-    cout << t_max << " " << (t1-t0) << endl;
-    cout << (num_flop / (t1-t0)/1e9) <<  " GFlops" << endl;
+    double deltaT = bench(nx,ny,t_max/N_FUSION);
+    double num_flop =  double(nx) * double(ny) * double(t_max) * 10 ;
+    cout << t_max << " " << deltaT << endl;
+    cout << (num_flop / deltaT/1e9) <<  " GFlops" << endl;
   }
 
 
