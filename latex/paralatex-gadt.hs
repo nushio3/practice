4bcdef16657ba9d49973213b87135aed625b0b33
nushio <nushio@nuwheezy.paraiso-lang.org>
2014-08-15 20:07:13 +0900

    no instance!

--- latex/paralatex-gadt.hs
+++ latex/paralatex-gadt.hs
@@ -1,6 +1,6 @@
-{-# LANGUAGE DataKinds, FlexibleContexts, GADTs, KindSignatures, OverloadedStrings, ScopedTypeVariables #-}
+{-# LANGUAGE DataKinds, FlexibleContexts, FlexibleInstances, GADTs, KindSignatures, OverloadedStrings, ScopedTypeVariables #-}
 
-
+import Data.Biapplicative
 import Data.Monoid
 import Data.String
 
@@ -8,31 +8,62 @@ data Expr (b :: Bool) s a where
   ERep :: { fromERep :: s } -> Expr False s a
   Expr :: { fromExpr :: a } -> Expr True s a
 
+instance Bifunctor (Expr True) where
+  bimap _ f (Expr a) = Expr (f a)
+instance Bifunctor (Expr False) where
+  bimap f _ (ERep s) = ERep (f s)
+  
 
+instance Biapplicative (Expr True) where
+  bipure s a = Expr a
+  ef <<*>> ea = Expr (fromExpr ef $ fromExpr ea)
 
+instance Biapplicative (Expr False) where
+  bipure s a = ERep s
+  ef <<*>> es = ERep (fromERep ef $ fromERep es)
 
-instance (Num a, IsString s, Monoid s) => Num (Expr b s a) where
-  ERep s1 + ERep s2 = ERep (s1 <> "+" <> s2)
+instance Num a => Num (Expr True s a) where
   Expr a1 + Expr a2 = Expr (a1 + a2)
-  ERep s1 * ERep s2 = ERep (s1 <> "*" <> s2)
   Expr a1 * Expr a2 = Expr (a1 * a2)
   
+instance (IsString s, Monoid s) => Num (Expr False s a) where
+  ERep s1 + ERep s2 = ERep (s1 <> "+" <> s2)
+  ERep s1 * ERep s2 = ERep (s1 <> "*" <> s2)
+
+
 
 paren :: (IsString s, Monoid s) => Expr b s a ->  Expr b s a 
 paren (Expr a) = Expr a
 paren (ERep s) = ERep $ "(" <> s <> ")"
 
-momentumX :: (Num a, IsString s, Monoid s) => Expr b s a ->  Expr b s a  ->  Expr b s a 
+momentumX :: (Num (Expr b s a)) => Expr b s a ->  Expr b s a  ->  Expr b s a 
 momentumX mass vel = mass * vel
 
-momentumF :: forall a. Num a => a -> a -> a
-momentumF m v = fromExpr $ momentumX (Expr m) (Expr v :: Expr True String a)
+momentumE :: forall a. Num a => a -> a -> a
+momentumE m v = fromExpr $ momentumX (Expr m) (Expr v)
 
-momentumE :: String -> String -> String
-momentumE m v = fromERep $ momentumX (ERep m) (ERep v :: Expr False String Double)
+momentumR :: String -> String -> String
+momentumR m v = fromERep $ momentumX (ERep m) (ERep v)
 
 
+solarMass :: Expr b String Double
+solarMass = bipure "Msun" 2.0e33
 
 main :: IO ()
 main = do
-  putStrLn "hi"
+  putStrLn $ "The momentum of particle with mass M, velocity V is : "
+    ++ momentumR "M" "V"
+  putStrLn $ "Therefore, momentum of particle with mass 6.0, velocity 7.0 is : "
+    ++ show (momentumE 6.0 7.0)
+    
+    
+{-
+
+paralatex-gadt.hs:50:13:
+    No instance for (Biapplicative (Expr b))
+      arising from a use of ‘bipure’
+    In the expression: bipure "Msun" 2.0e33
+    In an equation for ‘solarMass’: solarMass = bipure "Msun" 2.0e33
+
+
+-}    
