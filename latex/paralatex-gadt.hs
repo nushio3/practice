98a78dfb01cf768c33765206fa0b103b2c13788c
nushio <nushio@nuwheezy.paraiso-lang.org>
2014-08-15 19:49:02 +0900

    It is pity that we need type signatures here

--- latex/paralatex-gadt.hs
+++ latex/paralatex-gadt.hs
@@ -1,12 +1,14 @@
-{-# LANGUAGE DataKinds, FlexibleContexts, GADTs, KindSignatures, OverloadedStrings #-}
+{-# LANGUAGE DataKinds, FlexibleContexts, GADTs, KindSignatures, OverloadedStrings, ScopedTypeVariables #-}
 
 
 import Data.Monoid
 import Data.String
 
 data Expr (b :: Bool) s a where
-  ERep :: s -> Expr False s a
-  Expr :: a -> Expr True s a
+  ERep :: { fromERep :: s } -> Expr False s a
+  Expr :: { fromExpr :: a } -> Expr True s a
+
+
 
 
 instance (Num a, IsString s, Monoid s) => Num (Expr b s a) where
@@ -19,6 +21,17 @@ instance (Num a, IsString s, Monoid s) => Num (Expr b s a) where
 paren :: (IsString s, Monoid s) => Expr b s a ->  Expr b s a 
 paren (Expr a) = Expr a
 paren (ERep s) = ERep $ "(" <> s <> ")"
+
+momentumX :: (Num a, IsString s, Monoid s) => Expr b s a ->  Expr b s a  ->  Expr b s a 
+momentumX mass vel = mass * vel
+
+momentumF :: forall a. Num a => a -> a -> a
+momentumF m v = fromExpr $ momentumX (Expr m) (Expr v :: Expr True String a)
+
+momentumE :: String -> String -> String
+momentumE m v = fromERep $ momentumX (ERep m) (ERep v :: Expr False String Double)
+
+
 
 main :: IO ()
 main = do
