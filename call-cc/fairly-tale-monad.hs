2579ea682e5557cf12c693a892b8241962fe9eb2
Takayuki Muranushi <muranushi@gmail.com>
2015-04-28 12:37:51 +0900

    monad version complete.

--- call-cc/fairly-tale-monad.hs
+++ call-cc/fairly-tale-monad.hs
@@ -1,5 +1,14 @@
 {-# LANGUAGE RankNTypes, TypeSynonymInstances, ScopedTypeVariables #-}
 
+{-
+
+Wadler. http://homepages.inf.ed.ac.uk/wadler/papers/dual/dual.pdf
+Edward Yang. http://blog.ezyang.com/2013/04/a-classical-logic-fairy-tale/
+JRF.   http://jrf.cocolog-nifty.com/column/2011/01/post-1.html
+d.y.d. http://www.kmonos.net/wlog/61.html#_0538060508
+
+  -}
+
 import Control.Applicative
 import Control.Monad
 
@@ -12,6 +21,10 @@ instance Functor NN where
 instance Applicative NN where
   pure = return -- :: a -> NN a
   (<*>) = ap    -- :: NN (a -> b) -> NN a -> NN b
+
+unap :: (NN a -> NN b) -> NN (a -> b)
+unap f = undefined -- impossible, it seems.
+
 
 instance Monad NN where
     return x = NN ($ x)
@@ -33,9 +46,6 @@ exmid = exeither' exmid'
 
 {-
 
-callcc (
-   fun (k: (a, a->⊥) sum -> ⊥) ->
-         InRight (fun (x:a) -> k (InLeft x)))
 )
 -}
 
@@ -58,14 +68,16 @@ callCC2 f = NN $ \ c -> runNN
                         ((f :: (A -> NN B) -> NN A)
                          (\ x -> NN $ \ _ -> c (x :: A))) (c :: A -> B)
 
-nn :: ((A -> B) -> B) -> NN A
-nn = NN
 
-callCC3 :: ((A -> NN B) -> NN A) -> NN A
-callCC3 f = NN $ \ c ->  runNN
- ((f :: (A -> NN B) -> NN A)
-  (\ x -> NN $ \ _ -> c (x :: A))  :: NN A)
-     (c :: A -> B)
+
+proof3 :: NN (A -> B) -> (A -> NN B)
+proof3 nf a = nf <*> pure a
+
+-- callCCA :: ((NN A -> NN B) -> NN A) -> NN A
+-- callCCA f = NN $ \ c ->  runNN
+--  ((f :: (NN A -> NN B) -> NN A)
+--   (\ x -> \ _ -> c (x ::NN A))  :: NN A)
+--      (c :: A -> B)
 
 
 -- exmidA :: forall a. NN (Either a (a -> B))
@@ -79,22 +91,44 @@ callCC3 f = NN $ \ c ->  runNN
 
 
 data Stone
-type Life = B
-type Scroll = Stone -> Life
+type GodPower = B
+type ScrollOf a = a -> GodPower
 type Greater = NN
 
 
 
-cccA :: ((Stone -> NN B) -> Greater Stone) -> Greater Stone
+
+-- Greater Stone = (Stone -> GodPower) -> GodPower
+
+cccA :: ((Stone -> Greater GodPower) -> Greater Stone) -> Greater Stone
 cccA f = NN $ \ c -> runNN (f (\ x -> NN $ \ _ -> c x)) c
 
 
+-- Either a (a -> GodPower)
+ggg_eq_g :: Greater GodPower -> GodPower
+ggg_eq_g f = runNN f id
 
-resp :: NN (Either (Stone -> Greater Life) Stone)
+
+resp :: Greater (Either (Stone -> Greater GodPower) Stone)
 resp = exmid
 
 
+
+exmid2 :: NN (Either A (A -> NN B) )
+exmid2 = callCC f
+  where
+    f :: ((Either A (A -> NN B)) -> NN B) -> NN (Either A (A -> NN B))
+    f (k :: (Either A (A -> NN B)) -> NN B) = return $
+      (Right (\x -> k (Left x)))
+
+
 {-
+exmid
+callcc (
+   fun (k: (a, a->⊥) sum -> ⊥) ->
+         InRight (fun (x:a) -> k (InLeft x)))
+)
+
 
 -- "not (aまたはnot a) ならば (aまたはnot a)
 type Not a = a -> String
