39d9c99c4e579072b60d2078a0047a6a4ce66278
Takayuki Muranushi <muranushi@gmail.com>
2015-04-29 17:16:28 +0900

    very interesting call-cc.

--- call-cc/fairly-tale-contT.hs
+++ call-cc/fairly-tale-contT.hs
@@ -14,30 +14,48 @@ import Control.Monad
 import Control.Monad.IO.Class
 import Control.Monad.Cont
 
+-- Narrate the story
+story :: MonadIO m => String -> m ()
+story = liftIO . putStrLn
+
+-- A lot of money.
 data USD1G = USD1G deriving (Show)
-data BecomeGod
 
--- exmid :: (MonadCont m) => m (Either USD1G (USD1G -> m BecomeGod))
--- exmid = callCC f
---   where
---     f k = return (Right (\x -> k (Left x)))
+-- A forbidden, omnipotent object from which you can create anything
+newtype Anything = Anything {unleash :: forall a. a}
 
+-- The IO monad equipped with continuation
 type IOC = ContT () IO
 
-exmid :: IOC (Either USD1G (USD1G -> IOC BecomeGod))
-exmid = callCC f
+-- The devil's contract
+type Contract = Either USD1G (USD1G -> IOC Anything)
+
+-- The IO that signs and returns the contract
+exmid :: IOC Contract
+exmid = do
+  story "One day devil sais to you that either (L) he gives you $1G or (R) he will do anything you want if you give him $1G, but he get to choose."
+  story "You sign the contract. The story begins."
+  callCC f
   where
-    f k = return (Right (\x -> k (Left x)))
+--  f k = return (Right (\x -> k (Left x)))
+
+    f :: (Contract -> IOC Anything) -> IOC Contract
+    f k = do
+      story "Devil chooses (R)"
+      return (Right (\x ->
+                      story "Devil chooses (L)"
+                      >> k (Left x)))
 
 
-useTheWish :: Either USD1G (USD1G -> IOC BecomeGod) -> IO ()
-useTheWish e = (\m -> runContT m return ) $ case e of
+useTheWish :: Either USD1G (USD1G -> IOC Anything) -> IOC ()
+useTheWish e = case e of
   Left u -> do
-    liftIO $ putStrLn $ "Here I got money: " ++ show u
+    story $ "You got money: " ++ show u
   Right f -> do
-    liftIO $ putStrLn "Kami Ni Naru"
-    (g :: BecomeGod) <- f USD1G
-    liftIO $ putStrLn "Kami Korin "
+    story "You give the devil $1G to become a god"
+    (g :: Anything) <- f USD1G
+    story "You have become a omnipotent god"
+    story $ "You know that the answer to everything is: " ++ show (unleash g :: Integer)
 
 main :: IO ()
-main = runContT exmid useTheWish
+main = runContT (exmid >>= useTheWish) return
