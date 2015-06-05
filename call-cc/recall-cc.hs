9d7317031f487f0f9c1ad837e67bad1dda6ef1d3
Takayuki Muranushi <muranushi@gmail.com>
2015-06-06 00:27:49 +0900

    trap an wanted.

--- call-cc/recall-cc.hs
+++ call-cc/recall-cc.hs
@@ -1,6 +1,11 @@
 {-# LANGUAGE RankNTypes #-}
 
-newtype M a = M {unM :: forall r. (a -> r) -> r }
+newtype M a = M {runM :: forall r. (a -> r) -> r }
+
+
+callCC :: forall a b . ((a -> M b) -> M a) -> M a
+callCC f = M $ \ c -> runM (f (\ x -> M $ \ _ -> c x)) c
+
 
 
 fromM :: M a -> a
@@ -12,4 +17,4 @@ toM x = M (\k ->  k x)
 
 main :: IO ()
 main = do
-  print 1
+  print $ fromM $ toM 1
---
+++ typechecker-plugin/exe-src/Main.hs
@@ -0,0 +1,6 @@
+{-# OPTIONS_GHC -fplugin MyPlugin #-}
+
+data A = A
+
+main :: IO ()
+main = print A
