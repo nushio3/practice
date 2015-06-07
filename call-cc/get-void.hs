bc71002148950d0e304eda8e7bb37acb3a3f2165
Takayuki Muranushi <muranushi@gmail.com>
2015-06-07 21:29:49 +0900

    try many ways to produce Void

--- call-cc/get-void.hs
+++ call-cc/get-void.hs
@@ -1,11 +1,26 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 import Data.Void
 
 import Control.Monad.Cont
+import Control.Monad.IO.Class
 
-getEff :: ContT Void IO a -> IO Void
-getEff k = runContT (k >> return ()) ()
+getVoid :: ContT Void IO a -> IO Void
+getVoid k = runContT (k >> return ()) f
+  where
+    f :: () -> IO Void
+    f _ = runContT (k >> return ()) f
+
+getEff :: ContT Void IO a -> IO ()
+getEff k = runContT k f >> return ()
+  where
+    f :: a -> IO Void
+    f _ = return undefined
+
+ep12 :: ContT Void IO ()
+ep12 = do
+  liftIO $ putStrLn "Madoka : I am god"
+  liftIO $ putStrLn "Homura : Seriously?!"
 
 
 main :: IO ()
-main = do
-  print ""
+main = getEff ep12
