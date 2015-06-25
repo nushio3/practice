14b8887a4fd59768f34f7a87ddfdc9fe5e502dd4
Takayuki Muranushi <muranushi@gmail.com>
2015-06-25 16:58:50 +0900

    wrote my first optimizer ever in Hoopl.

--- hoopl/exe-src/main-1.hs
+++ hoopl/exe-src/main-1.hs
@@ -4,9 +4,15 @@ import IR
 prog1 :: Graph Insn C C
 prog1 = mkFirst (Entry ["a","b","c"])
         <*> mkMiddle (Assign "z" $ Triop FMA (Load "a") (Load "b") (Load "c"))
+        <*> mkMiddle (Assign "y" $ Triop FMA (Load "a") (Load "b") (Load "z"))
+        <*> mkMiddle (Assign "x" $ Binop Add (Load "z") (Load "y"))
         <*> mkLast (Return [Load "z"])
 
 
 main :: IO ()
 main = do
+  putStrLn "main"
   putStrLn $ showGraph show prog1
+
+  putStrLn "main -O1"
+  putStrLn $ showGraph show $ body $ optimize $ Proc "aaaa" prog1
--- hoopl/src/IR.hs
+++ hoopl/src/IR.hs
@@ -1,10 +1,10 @@
-{-# LANGUAGE GADTs,StandaloneDeriving,FlexibleInstances,FlexibleContexts #-}
+{-# LANGUAGE GADTs,StandaloneDeriving,FlexibleInstances,FlexibleContexts,RankNTypes,ScopedTypeVariables #-}
 
 module IR where
 
 import Compiler.Hoopl
 import Unsafe.Coerce
-
+import qualified Data.Set as S
 
 type Var = String
 
@@ -19,6 +19,13 @@ data Expr = Lit Double
           | Triop Triop Expr Expr Expr
                    deriving (Eq, Show)
 
+varIn :: Expr -> S.Set Var
+varIn x = case x of
+  Lit _ -> S.empty
+  Load v -> S.singleton v
+  Binop _ a b -> S.union (varIn a) (varIn b)
+  Triop _ a b c -> S.union (varIn a) $ S.union (varIn b) (varIn c)
+
 
 data Proc = Proc { name :: String, body :: Graph Insn C C }
 
@@ -27,9 +34,60 @@ data Insn e x where
   Assign :: Var    -> Expr    ->                    Insn O O
   Return :: [Expr] ->                               Insn O C
 
+onlyOneLabel :: Label
+onlyOneLabel = unsafeCoerce (0 :: Int)
+
 instance NonLocal Insn where
-  entryLabel (Entry _) = unsafeCoerce (0 :: Int)
+  entryLabel (Entry _) = onlyOneLabel
   successors (Return _) = []
 
 
 deriving instance Show (Insn e x)
+
+
+type Live = S.Set Var
+liveLattice :: DataflowLattice Live
+liveLattice = DataflowLattice
+  { fact_name = "Live variables"
+  , fact_bot  = S.empty
+  , fact_join = add
+  }
+    where add _ (OldFact old) (NewFact new) = (ch, j)
+            where
+              j = new `S.union` old
+              ch = changeIf (S.size j > S.size old)
+
+liveness :: BwdTransfer Insn Live
+liveness = mkBTransfer live
+  where
+    live :: Insn e x -> Fact x Live -> Live
+    live (Return x) _ = S.unions $ map varIn x
+    live (Assign v e) f
+      | v `S.member` f = S.union f (varIn e)
+      | otherwise      = f
+    live (Entry _) f = f
+
+
+deadCodeElim :: forall m . FuelMonad m => BwdRewrite m Insn Live
+deadCodeElim = mkBRewrite d
+  where
+    d :: Insn e x -> Fact x Live -> m (Maybe (Graph Insn e x))
+    d (Assign x _) live
+        | not (x `S.member` live) = return $ Just emptyGraph
+    d _ _ = return Nothing
+
+livePass :: BwdPass M Insn Live
+livePass = BwdPass{
+  bp_lattice = liveLattice,
+  bp_transfer = liveness,
+  bp_rewrite = deadCodeElim
+                  }
+
+
+type M = SimpleFuelMonad
+
+
+optimize :: Proc -> Proc
+optimize proc = runSimpleUniqueMonad $ runWithFuel 9999 $ do
+  (g,_,_) <- analyzeAndRewriteBwd livePass (JustC [onlyOneLabel]) (body proc) mapEmpty
+  return proc{body=g}
