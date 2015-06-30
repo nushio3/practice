c711189f62eee403a7ea333d82b6b87a346e6672
nushio <muranushi@gmail.com>
2015-06-30 23:43:37 +0900

    new plot with cmaes

--- chainer/test-optimizers.sh
+++ chainer/test-optimizers.sh
@@ -1,20 +1,28 @@
-./chainer-optimizer-test.py -i 3 -o AdaDelta &
-./chainer-optimizer-test.py -i 3 -o AdaGrad &
-./chainer-optimizer-test.py -i 3 -o Adam &
-./chainer-optimizer-test.py -i 3 -o MomentumSGD &
-./chainer-optimizer-test.py -i 3 -o RMSprop &
-./chainer-optimizer-test.py -i 3 -o SGD &         
+ ./chainer-optimizer-test.py -i 0 -o cmaes &
+ ./chainer-optimizer-test.py -i 1 -o cmaes &
+ ./chainer-optimizer-test.py -i 2 -o cmaes &
+ ./chainer-optimizer-test.py -i 3 -o cmaes &
+ ./chainer-optimizer-test.py -i 4 -o cmaes &
+ ./chainer-optimizer-test.py -i 5 -o cmaes &
 
-./chainer-optimizer-test.py -i 4 -o AdaDelta &
-./chainer-optimizer-test.py -i 4 -o AdaGrad &
-./chainer-optimizer-test.py -i 4 -o Adam &
-./chainer-optimizer-test.py -i 4 -o MomentumSGD &
-./chainer-optimizer-test.py -i 4 -o RMSprop &
-./chainer-optimizer-test.py -i 4 -o SGD &
 
-./chainer-optimizer-test.py -i 5 -o AdaDelta &
-./chainer-optimizer-test.py -i 5 -o AdaGrad &
-./chainer-optimizer-test.py -i 5 -o Adam &
-./chainer-optimizer-test.py -i 5 -o MomentumSGD &
-./chainer-optimizer-test.py -i 5 -o RMSprop &
-./chainer-optimizer-test.py -i 5 -o SGD &
+# ./chainer-optimizer-test.py -i 3 -o AdaDelta &
+# ./chainer-optimizer-test.py -i 3 -o AdaGrad &
+# ./chainer-optimizer-test.py -i 3 -o Adam &
+# ./chainer-optimizer-test.py -i 3 -o MomentumSGD &
+# ./chainer-optimizer-test.py -i 3 -o RMSprop &
+# ./chainer-optimizer-test.py -i 3 -o SGD &         
+# 
+# ./chainer-optimizer-test.py -i 4 -o AdaDelta &
+# ./chainer-optimizer-test.py -i 4 -o AdaGrad &
+# ./chainer-optimizer-test.py -i 4 -o Adam &
+# ./chainer-optimizer-test.py -i 4 -o MomentumSGD &
+# ./chainer-optimizer-test.py -i 4 -o RMSprop &
+# ./chainer-optimizer-test.py -i 4 -o SGD &
+# 
+# ./chainer-optimizer-test.py -i 5 -o AdaDelta &
+# ./chainer-optimizer-test.py -i 5 -o AdaGrad &
+# ./chainer-optimizer-test.py -i 5 -o Adam &
+# ./chainer-optimizer-test.py -i 5 -o MomentumSGD &
+# ./chainer-optimizer-test.py -i 5 -o RMSprop &
+# ./chainer-optimizer-test.py -i 5 -o SGD &
