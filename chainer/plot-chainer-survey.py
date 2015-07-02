ee7bec6c949ad9fee8b03f5112a535ec19cbf141
nushio <muranushi@gmail.com>
2015-07-03 08:20:44 +0900

    x

--- chainer/plot-chainer-survey.py
+++ chainer/plot-chainer-survey.py
@@ -5,10 +5,10 @@ import subprocess
 def system(cmd):
     subprocess.call(cmd, shell=True)
 
-optimizers = "AdaDelta AdaGrad Adam MomentumSGD RMSprop SGD cmaes".split()
+optimizers = "cmaes AdaDelta AdaGrad Adam MomentumSGD RMSprop SGD".split()
 
 plots = []
-color="#80cc00 #00cc80 #00ff00 #ff0000 #0000ff #800000 #ffff00".split()
+color="#c0c0c0 #00ff00 #0000ff #00ffff #ff00ff #ffff00 #ff0000".split()
 
 for o in range(7):
     for i in range(6):
@@ -28,5 +28,6 @@ plot {plotsH}
 """.format(plotsH = ','.join(plots)))
 
 system("gnuplot tmp.gnu")
+system("ps2pdf optimization-progress.eps")
 
 
