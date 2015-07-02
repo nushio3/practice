2791688e0ccd6bf798bbb77466def9607295746b
Takayuki Muranushi <muranushi@gmail.com>
2015-07-02 22:48:12 +0900

    change order of drawing

--- chainer/plot-chainer-survey.py
+++ chainer/plot-chainer-survey.py
@@ -5,10 +5,10 @@ import subprocess
 def system(cmd):
     subprocess.call(cmd, shell=True)
 
-optimizers = "AdaDelta AdaGrad Adam MomentumSGD RMSprop SGD cmaes".split()
+optimizers = "cmaes AdaDelta AdaGrad Adam MomentumSGD RMSprop SGD".split()
 
 plots = []
-color="#80cc00 #00cc80 #00ff00 #ff0000 #0000ff #800000 #ffff00".split()
+color="#ffff00 #80cc00 #00cc80 #00ff00 #ff0000 #0000ff #800000".split()
 
 for o in range(7):
     for i in range(6):
@@ -17,7 +17,7 @@ for o in range(7):
         plots += [plot]
 
 with open("tmp.gnu", "w") as fp:
-    fp.write(""" 
+    fp.write("""
 set term postscript enhanced color eps solid 20
 set out "optimization-progress.eps"
 set grid
@@ -28,5 +28,3 @@ plot {plotsH}
 """.format(plotsH = ','.join(plots)))
 
 system("gnuplot tmp.gnu")
-
-
