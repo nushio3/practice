f21d32a7633113a474bfe1d4b817bdd245217295
Takayuki Muranushi <muranushi@gmail.com>
2016-01-27 11:33:02 +0900

    practice haskell!

--- crawler/app/Main.hs
+++ crawler/app/Main.hs
@@ -1,6 +1,16 @@
 module Main where
 
+import Control.Monad
+import Network.Browser
+import Network.HTTP
 import Lib
 
+getURL :: String -> IO String
+getURL url = do
+  resp <- simpleHTTP $ getRequest url
+  getResponseBody resp
+
 main :: IO ()
-main = someFunc
+main = do
+  con <- getURL "http://www.google.com"
+  putStrLn $ con
