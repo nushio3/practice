68a28f6b9f339f35147447617a568f5d59ecff3e
Takayuki Muranushi <muranushi@gmail.com>
2016-01-31 20:33:37 +0900

    temporalily save the crawler.

--- crawler/app/Main.hs
+++ crawler/app/Main.hs
@@ -1,19 +1,57 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving, OverloadedStrings #-}
+
 module Main where
 
+import Control.Applicative
 import Control.Monad
-import Network.Browser
-import Network.HTTP
-import Lib
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Either
+import Data.Maybe (catMaybes)
+import Network.Curl (curlGetString, CurlOption(..))
+import Text.PrettyPrint.ANSI.Leijen(putDoc)
+import Text.HTML.TagSoup
+import qualified Data.Text as T
 
-getURL :: String -> IO String
+
+
+newtype NAM a = NAM {runNAM :: EitherT String IO a}
+              deriving (Functor, Applicative, Monad, MonadIO)
+
+getURL :: String -> NAM ()
 getURL url = do
-  resp <- simpleHTTP $ getRequest url
+  (code, resp) <- liftIO $ curlGetString url [CurlFollowLocation True]
   case resp of
-    Left err -> return ""
-    Rigt
-  getResponseBody resp
+    "" -> NAM $ left $ show code
+    _  -> do
+      let tags :: [Tag T.Text]
+          tags = parseTags $ T.pack resp
+      liftIO $ mapM_ print $ imgURLs $ map head $ sections (~== ("<img>"::String)) tags
+      liftIO $ mapM_ print $ webURLs $ map head $ sections (~== ("<a>"::String)) tags
+
+  where
+    imgURLs :: [Tag T.Text] -> [] T.Text
+    imgURLs tags = do
+      TagOpen _ attrs <- tags
+      ("src", imgURL) <- attrs
+      return imgURL
+
+    webURLs :: [Tag T.Text] -> [] T.Text
+    webURLs tags = do
+      TagOpen _ attrs <- tags
+      ("href", url) <- attrs
+      return url
+
+catScore :: T.Text -> Int
+catScore txt = sum [factor * T.count kwd txt | (kwd, factor) <- keywords]
+  where
+    keywords = [("猫", 10),
+                ("子猫", 100),
+                ("かわいい", 50),
+                ("cat", 10),
+                ("kitten", 28)        ]
+
 
 main :: IO ()
 main = do
-  con <- getURL "http://www.google.com"
-  putStrLn $ con
+  ret <- runEitherT $ runNAM $ getURL "http://matome.naver.jp/topic/1M2IY"
+  print ret
